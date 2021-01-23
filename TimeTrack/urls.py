7cc1e3c8459abe1bba533979dfda3d7a64c7fc80
Sehrishah <sehrishwaheed98@gmail.com>
2021-01-23 19:47:29 +0300

    Edit profile

--- TimeTrack/urls.py
+++ TimeTrack/urls.py
@@ -14,11 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.contrib.auth import views as auth_views
 
 from apps.core.views import frontpage, privacy, terms, plans
-from apps.userprofile.views import signup, myaccount
+from apps.userprofile.views import signup
 
 
 
@@ -32,8 +32,9 @@ urlpatterns = [
     # Auth
     path('signup/', signup, name="signup"),
     path('login/', auth_views.LoginView.as_view(template_name="userprofile/login.html"),  name="login"),
-    path('myaccount/', myaccount, name="myaccount"),
     path('logout/', auth_views.LogoutView.as_view(), name="logout"),
+    path('myaccount/', include('apps.userprofile.urls')),
+   
 ]
 
 COMPRESS_PRECOMPILERS = (
--- apps/userprofile/views.py
+++ apps/userprofile/views.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import login
@@ -39,3 +40,16 @@ def signup(request):
 def myaccount(request):
     return render(request, 'userprofile/myaccount.html')
 
+@login_required
+def edit_profile(request):
+    if request.method == 'POST':
+        request.user.first_name = request.POST.get('first_name', '')
+        request.user.last_name = request.POST.get('last_name', '')
+        request.user.email = request.POST.get('email', '')
+        request.user.save()
+
+        messages.info(request, 'The changes was saved')
+
+        return redirect('myaccount')
+
+    return render(request, 'userprofile/edit_profile.html')
