7334a6876ddee8f3b4a800c444398c08efaa83f0
Sehrishah <sehrishwaheed98@gmail.com>
2021-01-25 12:40:37 +0300

    Show userprofile in admin area

--- apps/userprofile/admin.py
+++ apps/userprofile/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+# Show userprofile in admin area
+#import models
+from .models import Userprofile
+
+
+# register
+admin.site.register(Userprofile)
