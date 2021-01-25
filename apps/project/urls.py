34241c27644878c1ff275f1b6b0417a8aeb4e839
Sehrishah <sehrishwaheed98@gmail.com>
2021-01-25 16:23:26 +0300

    Show detail of prohect

--- apps/project/urls.py
+++ apps/project/urls.py
@@ -2,10 +2,11 @@
 
 from django.urls import path
 
-from .views import projects
+from .views import projects, project
 
 # Url pattern
 app_name = 'project'
 urlpatterns = [
-    path('', projects, name="projects")
+    path('', projects, name="projects"),
+    path('<int:project_id>/', project, name='project')
 ]
--- apps/project/views.py
+++ apps/project/views.py
@@ -23,3 +23,12 @@ def projects(request):
             return redirect('project:projects')
 
     return render(request, 'project/projects.html', {'team': team, 'projects': projects})
+
+
+@login_required
+def project(request, project_id):
+    team = get_object_or_404(
+        Team, pk=request.user.userprofile.active_team_id, status=Team.ACTIVE)
+    project = get_object_or_404(Project, team=team, pk=project_id)
+
+    return render(request, 'project/project.html', {'team': team, 'project': project})
