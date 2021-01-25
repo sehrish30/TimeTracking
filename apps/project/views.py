9e4600fab4aa49e397c76486826be2bc2c84b9d6
Sehrishah <sehrishwaheed98@gmail.com>
2021-01-25 18:22:00 +0300

    Show a list of tasks

--- apps/project/views.py
+++ apps/project/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
-from .models import Project
+from .models import Project, Task
 from apps.team.models import Team
 
 
@@ -33,7 +33,20 @@ def project(request, project_id):
         Team, pk=request.user.userprofile.active_team_id, status=Team.ACTIVE)
     project = get_object_or_404(Project, team=team, pk=project_id)
 
-    return render(request, 'project/project.html', {'team': team, 'project': project})
+    if request.method == 'POST':
+        title = request.GET.get('title')
+
+        if title:
+            task = Task.objects.create(team=team, project=project, title=title)
+            messages.info(request, 'Task Added')
+
+            # redirect the user back to product page
+            return redirect('project:project', project_id=project_id)
+    # get the tasks to show on project page
+    tasks_todo = project.tasks.filter(status=Task.TODO)
+    tasks_done = project.tasks.filter(status=Task.DONE)
+
+    return render(request, 'project/project.html', {'team': team, 'project': project, 'tasks_todo': tasks_todo, 'tasks_done': tasks_done})
 
 
 @login_required
