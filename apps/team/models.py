21cedb3cbbee186fda9ed5eb5b64d60cb77d970b
Sehrishah <sehrishwaheed98@gmail.com>
2021-01-27 16:40:04 +0300

    Create a model for inviations

--- apps/team/models.py
+++ apps/team/models.py
@@ -2,21 +2,24 @@ from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 
+
 class Team(models.Model):
     ACTIVE = 'active'
     DELETED = 'deleted'
 
     CHOICES_STATUS = (
-        {ACTIVE, 'Active'},
-        {DELETED, 'Deleted'}
+        (ACTIVE, 'Active'),
+        (DELETED, 'Deleted')
     )
 
     # Fields
-    title = models.CharField(max_length = 255);
+    title = models.CharField(max_length=255)
     members = models.ManyToManyField(User, related_name='teams')
-    created_by = models.ForeignKey(User, related_name="created_teams", on_delete = models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add = True)
-    status = models.CharField(max_length=10, choices = CHOICES_STATUS, default = ACTIVE)
+    created_by = models.ForeignKey(
+        User, related_name="created_teams", on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(
+        max_length=10, choices=CHOICES_STATUS, default=ACTIVE)
 
    # sorted by title
     class Meta:
@@ -24,3 +27,24 @@ class Team(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Invitation(models.Model):
+    INVITED = 'invited'
+    ACCEPTED = 'accepted'
+
+   # choices is iterable containing actual values and human readable values
+    CHOICES_STATUS = (
+        (INVITED, 'Invited'),
+        (ACCEPTED, 'Accepted')
+    )
+    team = models.ForeignKey(
+        Team, related_name='invitations', on_delete=models.CASCADE)
+    email = models.EmailField()
+    code = models.CharField(max_length=20)
+    status = models.CharField(
+        max_length=20, choices=CHOICES_STATUS, default=INVITED)
+    date_sent = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.email
