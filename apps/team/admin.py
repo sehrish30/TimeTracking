6e4ead478313f2b94e62a161bbf67122fa63dd38
Sehrishah <sehrishwaheed98@gmail.com>
2021-01-30 13:30:49 +0300

    Create a new model for plans and update the team model

--- apps/team/admin.py
+++ apps/team/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Team, Invitation
+from .models import Team, Invitation, Plan
 
 admin.site.register(Team)
 admin.site.register(Invitation)
+admin.site.register(Plan)
--- apps/team/models.py
+++ apps/team/models.py
@@ -3,6 +3,18 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 
+class Plan(models.Model):
+    title = models.CharField(max_length=255)
+    max_projects_per_team = models.IntegerField(default=0)
+    max_members_per_team = models.IntegerField(default=0)
+    max_tasks_per_project = models.IntegerField(default=0)
+    price = models.IntegerField(default=0)
+    is_default = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
+
+
 class Team(models.Model):
     ACTIVE = 'active'
     DELETED = 'deleted'
@@ -10,6 +22,15 @@ class Team(models.Model):
     CHOICES_STATUS = (
         (ACTIVE, 'Active'),
         (DELETED, 'Deleted')
+    )
+
+    # plan status
+    PLAN_ACTIVE = 'active'
+    PLAN_CANCELLED = 'cancelled'
+
+    CHOICES_PLAN_STATUS = (
+        (PLAN_ACTIVE, 'Active'),
+        (PLAN_CANCELLED, 'Cancelled')
     )
 
     # Fields
@@ -20,6 +41,15 @@ class Team(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     status = models.CharField(
         max_length=10, choices=CHOICES_STATUS, default=ACTIVE)
+    plan = models.ForeignKey(Plan, related_name='teams',
+                             on_delete=models.CASCADE)
+    plan_end_date = models.DateTimeField(blank=True, null=True)
+    plan_status = models.CharField(
+        max_length=20, choices=CHOICES_PLAN_STATUS, default=PLAN_ACTIVE)
+    stripe_customer_id = models.CharField(
+        max_length=255, blank=True, null=True)
+    stripe_subscription_id = models.CharField(
+        max_length=255, blank=True, null=True)
 
    # sorted by title
     class Meta:
